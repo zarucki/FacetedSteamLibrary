2a590b03d07bad45dc1d024bdd72fc20f3906376
zarucki <zarucki@gmail.com>
2014-01-25 19:13:38 +0100

    Node.js server which is acting as a proxy to bypass same origin problem. For now only fetching the library of user.

---
+++ server.js
@@ -0,0 +1,67 @@
+var http = require('http');
+var util = require('util');
+var url = require('url');
+
+var app = http.createServer(handler);
+app.listen(89);
+
+var steamApiKey = getSteamApiKey();
+
+function getSteamApiKey()
+{
+  var fs = require('fs');
+  var contentsOfFile = fs.readFileSync('steam_api_key', { encoding: 'utf8' });
+  return contentsOfFile.replace(/[^a-zA-Z0-9]/g, '');
+}
+
+function getSteamUserLibraryList(steamUserId, response) {
+  response.setHeader("Content-Type", "application/json");
+
+  var path = util.format('%s?key=%s&steamid=%s&format=json&include_appinfo=1&include_played_free_games=0', 
+      '/IPlayerService/GetOwnedGames/v0001/',
+      steamApiKey,
+      steamUserId);
+
+  http.get({host: 'api.steampowered.com', port: 80, path: path}, function(res) {
+    if(res.statusCode !== 200) {
+      console.log(res.statusCode);
+      response.writeHead(res.statusCode);
+      response.end();
+      return;
+    }
+
+    var result = '';
+    response.writeHead(200);
+    // TODO: should i worry about encoding?
+    // res.setEncoding('binary');
+
+    res.on('data', function(chunk) {
+      // encoding here is optional parameter
+      response.write(chunk)
+    });
+
+    res.on('end', function() {
+      console.log('done');
+      response.end();
+    });
+
+  });
+}
+
+function handler (req, res) {
+  var queryData = url.parse(req.url, true).query;
+  console.log(queryData);
+
+  // I don't give a fuck origin policy for testing purposes, when actually hosted you would
+  // rather like to limit it to your own site so noone abuses your service
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (queryData.steamUserId) {
+    getSteamUserLibraryList(queryData.steamUserId, res);
+  } else {
+    res.writeHead(403);
+    res.end();
+  }
+}
